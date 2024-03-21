8ad10ac13f530c7d294e9a2865c6cbdcf0dcf66d
Stijn <stijn@forcai.eu>
2024-03-21 21:11:48 +0100

    oke

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ import random
 st.header('📖 Woordjes')
 
 
-  
+with st.sidebar:
+    st.write(st.session_state)  
 
 
 if 'woordjes' not in  st.session_state:
     st.session_state['woordjes'] = None
     
 if 'placeholder' not in  st.session_state:
-    st.session_state['placeholder'] = True
-    
-    
+    st.session_state['placeholder'] = True    
     
 if 'history' not in  st.session_state:
     st.session_state['history'] = None
@@ -27,43 +26,21 @@ if 'finished' not in  st.session_state:
     st.session_state['finished'] = False
 
 
-    
-
-
-#with st.sidebar:
-#    st.write(st.session_state)
-
-
-
 tabs = st.tabs(['Woordenlijst','Oefenen'])
-
-
-
 
 
 with tabs[0]:
     
-    
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-  
     else:
-        df = pd.DataFrame(
-        [
-        {"Nederlands": "Hallo", "Spaans":'Hola'},
-    ]
-    )
-    
+        df = pd.DataFrame([{"Nederlands": "Hallo", "Spaans":'Hola'}])
     
     edited_df = st.data_editor(df, num_rows='dynamic')
-
-
     df = edited_df.sample(frac = 1).reset_index(drop=True)
     
     if st.button('Sla op'):
-
-       
         st.session_state['woordjes'] = df
         st.session_state['history'] = {'goed':0,'fout':0, 'te gaan':len(st.session_state['woordjes']['Nederlands'])}
         st.session_state['index'] = 0
@@ -100,59 +77,41 @@ with tabs[1]:
         
         with cols[0]:
             
+            # Het is nog niet afgerond
             if not st.session_state['finished']:
                 with st.container(border=True):
                     
-                    index = st.session_state['index']
-                    
                     invoer = st.text_input(label=woord_van)
 
-                    check = st.button('check')
-                    
-                    if check:
+                    # De knop is ingedrukt
+                    if st.button('check'):
                         
                         is_goed = invoer.lower()==woord_naar.lower()
-                        nu_de_laatste = index + 1  == len(dic['Spaans'])
+                        nu_de_laatste = st.session_state['index'] + 1 == len(dic['Spaans'])
                         
-                        if nu_de_laatste:
-                            st.session_state['finished'] = True
-                            if is_goed:
-                                st.session_state['history']['goed'] += 1
-                                st.session_state['history']['te gaan'] -= 1
-                                
-                                st.success('Nice')
-                                sleep(0.5)
-                                st.rerun()
-                            else:
-                                st.session_state['history']['fout'] += 1
-                                st.session_state['history']['te gaan'] -= 1
-                                
-                                st.error(f"{woord_naar}")
-                                sleep(2)
-                                st.rerun()
-
-                        # Niet de laatste                            
-                        else:
-                            pass
-                            print(3)
-                            
+                        # Dit moet sws
+                        st.session_state['history']['te gaan'] -= 1
                         
+                        # Is het goed?
                         if is_goed:
                             st.session_state['history']['goed'] += 1
-                            st.session_state['history']['te gaan'] -= 1
-                            st.session_state['index'] += 1
                             st.success('Nice')
-                            sleep(0.5)
-                            st.rerun()
+                            sleep(1)
                         else:
                             st.session_state['history']['fout'] += 1
-                            st.session_state['history']['te gaan'] -= 1
-                            st.session_state['index'] += 1
-                            st.error(f"{woord_naar}")
+                            st.error(woord_naar)
                             sleep(2)
-                            st.rerun()
-          
-           
+                        
+                        # De laatste?    
+                        if not nu_de_laatste:
+                            st.session_state['index'] += 1
+                        else:
+                            st.session_state['finished'] = True
+                        
+                        # Alles gedaan, dan refreshen
+                        st.rerun()    
+                        
+
             # wel klaar
             else:
                 st.info('Het zit erop gap')
@@ -169,13 +128,25 @@ with tabs[1]:
             
         with cols[1]:    
             with st.container(border=True):
-                st.write('Stats')
+                st.write('Verhouding')
 
                 his =  st.session_state['history']
                 
                 df2 = pd.DataFrame({0:{'Goed':his['goed'], 'Fout':his['fout']}}).T
                 
-                st.dataframe(df2, hide_index=True)
+                
+                bar_goed = st.progress(0, text='voortgang')
+                bar_fout = st.progress(0, text='voortgang')
+                
+                if st.session_state['index'] == 0:
+                    bar_goed.progress(his['goed']/ (st.session_state['index']+1), f"{his['goed']} goed")
+                    bar_fout.progress(his['fout']/ (st.session_state['index']+1), f"{his['fout']} fout")
+                else:
+                    bar_goed.progress(his['goed']/ (st.session_state['index']), f"{his['goed']} goed")
+                    bar_fout.progress(his['fout']/ (st.session_state['index']), f"{his['fout']} fout")
+                
+                
+                #st.dataframe(df2, hide_index=True)
  
  
         my_bar = st.progress(0, text='voortgang')
@@ -186,4 +157,3 @@ with tabs[1]:
     else:
         st.error('Sla eerst je woordjes op')
             
-
