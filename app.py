702f4da82d9155a0ece347615abf9e6f16ff8cca
Stijn <stijn@forcai.eu>
2024-03-21 15:24:33 +0100

    oke

--- app.py
+++ app.py
@@ -127,15 +127,15 @@ with tabs[1]:
                 col1, col2, col3 = st.columns(3)
                 
                 with col1:
-                    st.header(his['goed'])
+                    st.subheader(his['goed'])
                     st.write("goed")
                     
                 with col2:
-                    st.header(his['fout'])
+                    st.subheader(his['fout'])
                     st.write("fout")
                 
                 with col3:
-                    st.header(his['te gaan'])
+                    st.subheader(his['te gaan'])
                     st.write("te gaan")
                 
                 
